01f9d7f2b96eb12ee12ddfbe4f3e6266ea5a4900
john-abou <abounassar.john@gmail.com>
2023-03-29 09:54:37 -0400

    Revert "Refactor: Removed service worker as it is not needed."
    This reverts commit cb52990cdcf6ed28f1c683b8df860c104d0d6852.

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// StrictMode is used for development purposes only -- it will help you find potential problems in your application
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
