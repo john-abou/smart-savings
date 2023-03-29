864e4a8758103467b7f41fc562c43e1280a31457
Mdwag316 <mandeepa98@gmail.com>
2023-03-29 15:45:48 -0700

    created mutation to scrape and update database, cleaned up scrape code to formate price

--- server/Schema/resolvers.js
+++ server/Schema/resolvers.js
@@ -2,6 +2,10 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Order, Product } = require('../models');
 const { signToken } = require('../utils/auth');
 
+const { searchAmazon } = require('../utils/scrape/searchAmazon');
+const { searchWal } = require('../utils/scrape/searchWal');
+const { searchLoblaws } = require('../utils/scrape/searchLoblaws');
+
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
@@ -68,6 +72,34 @@ const resolvers = {
         return updatedOrder;
       }
       throw new AuthenticationError('You need to be logged in!');
+    },
+
+    appendCompetitiorPriceHistory: async (parent, { productId }, context) => {
+      const amazonLink = await Product.findOne({ _id: productId }).select('AmazonHistory.link');
+      const walmartLink = await Product.findOne({ _id: productId }).select('WalmartHistory.link');
+      const loblawsLink = await Product.findOne({ _id: productId }).select('LoblawsHistory.link');
+
+      const amazonPrice = await searchAmazon(amazonLink.AmazonHistory.link);
+      console.log(amazonPrice);
+      const walmartPrice = await searchWal(walmartLink.WalmartHistory.link);
+      console.log(walmartPrice);
+      const loblawsPrice = await searchLoblaws(loblawsLink.LoblawsHistory.link);
+      console.log(loblawsPrice);
+
+      // find one and add
+      const updatedProduct = await Product.findOneAndUpdate(
+        { _id: productId },
+        {
+          $addToSet: {
+            "AmazonHistory.priceHistory": { price: amazonPrice },
+            "WalmartHistory.priceHistory": { price: walmartPrice },
+            "LoblawsHistory.priceHistory": { price: loblawsPrice }
+          }
+        },
+        { new: true });
+
+      return updatedProduct;
+
     }
   }
 };
--- server/utils/scrape/searchLoblaws.js
+++ server/utils/scrape/searchLoblaws.js
@@ -2,19 +2,23 @@ const puppeteer = require("puppeteer-extra")
 const StealthPlugin = require("puppeteer-extra-plugin-stealth")
 const { executablePath } = require("puppeteer")
 
+const { formatPrice } = require("./formatPrice")
+
 const searchLoblaws = async (searchLink) => {
     puppeteer.use(StealthPlugin())
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         executablePath: executablePath()
     })
     const page = await browser.newPage()
     await page.goto(searchLink)
     await page.waitForSelector("#site-content > div > div > div.product-tracking > div.product-details-page-details > div.product-details-page-details__content__name > div > div > div.product-details-page-details__content__sticky-placeholder > div > div.product-details-page-details__content__prices > div > div > div > span > span.price__value.selling-price-list__item__price.selling-price-list__item__price--now-price__value")
-    const result = await page.evaluate(() => {
+    var result = await page.evaluate(() => {
         const price = document.querySelector("#site-content > div > div > div.product-tracking > div.product-details-page-details > div.product-details-page-details__content__name > div > div > div.product-details-page-details__content__sticky-placeholder > div > div.product-details-page-details__content__prices > div > div > div > span > span.price__value.selling-price-list__item__price.selling-price-list__item__price--now-price__value").innerText
         return price
     })
+
+    result = formatPrice(result)
 
     await browser.close()
     return result
@@ -22,6 +26,7 @@ const searchLoblaws = async (searchLink) => {
 
 
 // Can Pass In Any Search Term We Want ----- Test With This -----
+//let productLink = "https://www.loblaws.ca/original-toilet-paper-12-24-rolls-242-bath-tissues/p/21363770_EA"
+//searchLoblaws(productLink).then((result) => console.log(result))
 
-let productLink = "https://www.loblaws.ca/original-toilet-paper-12-24-rolls-242-bath-tissues/p/21363770_EA"
-searchLoblaws(productLink).then((result) => console.log(result))
+module.exports = { searchLoblaws }
