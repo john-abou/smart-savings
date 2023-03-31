20e82cefaa41aab79d2fe68fd41e1b54017676bb
john-abou <abounassar.john@gmail.com>
2023-03-30 21:08:30 -0400

    Feat: Cart state management through productItem.

--- client/src/components/User/ProductContainer/index.js
+++ client/src/components/User/ProductContainer/index.js
@@ -1,80 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
+// Import React
+import React, { useEffect, useReducer } from 'react';
+// Import statemanagement hooks and reducers/actions
+import { useStoreContext } from '../../../contexts/GlobalContext';
+import { reducer } from '../../../utils/reducers';
+import { UPDATE_PRODUCTS } from '../../../utils/actions';
+// Import apollo hooks and queries
 import { useQuery } from '@apollo/client';
 import { QUERY_ALL_PRODUCTS } from '../../../utils/queries';
-
+// Import ProductItem component
 import ProductItem from '../ProductItem';
 
 
 
 export default function ProductContainer() {
+  const initialState = useStoreContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  // const [showProducts, setShowProducts] = useState(); 
-
-  const  { data }  = useQuery(QUERY_ALL_PRODUCTS);
-
-  // console.log(data);
+  const { data }  = useQuery(QUERY_ALL_PRODUCTS);
 
   useEffect(() => {
-  const products = data?.products || [];
-  
-  console.log(products);
-
-  // setShowProducts(products);
-
-  // //console.log(typeof (products))
-  },[data]);
-
-  const products = [
-    {
-      _id: 1,
-      name: 'Product 1',
-      description: 'Product 1 description',
-      price: 10,
-      image: 'https://via.placeholder.com/150',
-      quantity: 10,
-      category: 'Category 1'
-    },{
-      _id: 2,
-      name: 'Product 2',
-      description: 'Product 2 description',
-      price: 20,
-      image: 'https://via.placeholder.com/150',
-      quantity: 20,
-      category: 'Category 2'
-    },{
-      _id: 3,
-      name: 'Product 3',
-      description: 'Product 3 description',
-      price: 30,
-      image: 'https://via.placeholder.com/150',
-      quantity: 30,
-      category: 'Category 3'
-    },{
-      _id: 4,
-      name: 'Product 4',
-      description: 'Product 4 description',
-      price: 40,
-      image: 'https://via.placeholder.com/150',
-      quantity: 40,
-      category: 'Category 4'
-    },{
-      _id: 5,
-      name: 'Product 5',
-      description: 'Product 5 description',
-      price: 50,
-      image: 'https://via.placeholder.com/150',
-      quantity: 50,
-      category: 'Category 5'
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+    } else {
+      console.log('No data found');
     }
-  ];
+  }, [data, dispatch]);
 
   return (
     <section id='products' className='container-fluid'>
       <div className='row'>
         {
-          products.map((product) => (
+          state.products.map((product) => (
             <ProductItem key={product._id} product={product} />
           ))
         }
--- client/src/components/User/ProductItem/index.js
+++ client/src/components/User/ProductItem/index.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { useStoreContext } from '../../../contexts/GlobalContext';
 import { reducer } from '../../../utils/reducers';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY, REMOVE_FROM_CART } from '../../../utils/actions';
@@ -15,51 +15,44 @@ export default function ProductItem( { product } ) {
   // Define a function to handle add to cart, should add item to cart and increase cart count in global state
   const addToCart = () => { 
     // Determine if the item is in the cart, then add to cart and update quantity
-    /* const itemInCart = cart.find((item) => item._id === product._id)
-
-    if (itemInCart) {
+    const productInCart = cart.find( (cartProduct) => cartProduct._id === product._id);
+    console.log(productInCart);
+    if (productInCart && productInCart.purchaseCount > 0) {
       dispatch({
-        type: UPDATE_CART_AMOUNT,
-        _id: product._id,
-        purchaseQuantity: parseInt(product.quantity) + 1
-      })
+        type: UPDATE_CART_QUANTITY,
+        _id: productInCart._id,
+        purchaseCount: parseInt(productInCart.purchaseCount) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...product, purchaseCount: 1 }
+      });
     }
-    dispatch({
-      type: ADD_TO_CART,
-      product: { ...product, purchaseQuantity: 1 }
-    }); */
-
-    cart.forEach(item => {
-      console.log('item_id:', item._id);
-      console.log('prop_id', product._id);
-      console.log('if test: ', item._id === product._id);
-      if (item._id === product._id) {
-        // If the item is in the cart, update the cart quantity
-        console.log('made it inside the if block')
-        dispatch({
-          type: UPDATE_CART_QUANTITY,
-          _id: product._id,
-          purchaseQuantity: parseInt(item.purchaseQuantity) + 1
-        });
-        // Exit the function
-        return;
-      } else {
-        console.log("cart behind 1 click: ", cart);
-        // If the item is not in the cart, add it to the cart
-        dispatch({
-          type: ADD_TO_CART,
-          product: { ...product, purchaseQuantity: 1 }
-        });
-      }
-    });
+    // state.product.quantity--;
   }
 
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   const removeFromCart = () => {
-    // Remove the item from the cart
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: product._id
-    });
+    // If the item clicked matches the item in the cart, remove it from the cart
+    const productInCart = cart.find( (cartProduct) => cartProduct._id === product._id);
+    console.log(productInCart);
+    if (productInCart.purchaseCount === 1) {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: productInCart._id
+      });
+    } else {
+      console.log('Item in cart:', parseInt(productInCart.purchaseCount) - 1);
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: productInCart._id,
+        purchaseCount: parseInt(productInCart.purchaseCount) - 1 // Update item in cart
+      });
+    }
   }
 
 
