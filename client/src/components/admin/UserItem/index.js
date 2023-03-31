565872a5529b257b00274b153799f626db292355
john-abou <abounassar.john@gmail.com>
2023-03-31 14:41:39 -0400

    Feat: Admin management of user accounts -- incomplete -- user component renders but update to db has a bug.

--- client/src/components/admin/UserItem/index.js
+++ client/src/components/admin/UserItem/index.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import { useStoreContext } from '../../contexts/GlobalContext';
+import { useStoreContext } from '../../../contexts/GlobalContext';
+import { UPDATE_USER } from '../../../utils/mutations';
+import { UPDATE_USERS } from '../../../utils/actions';
+import { useMutation } from '@apollo/client';
 
 export default function UserItem( {user} ) {
   const [state, dispatch] = useStoreContext();
   const { users } = state;
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    dispatch({
-      type: UPDATE_USERS,
-      users: [...users, { [name]: value }],
-    });
-  };
 
+  const [updateUsers, {error}] = useMutation(UPDATE_USER);
+  
+
+  const handleEditClick = (e) => {
+    // Get input from the user to confirm the account type
+    const accountType = prompt('Enter the account type of the user: (regular/admin)');
+    // If the user enters a valid account type, update the user's account type
+    if (accountType === 'regular' || accountType === 'admin') {
+      // Update the user's account type in the database
+      try{ 
+        const { data } = updateUsers({
+        variables: {
+          _id: user._id,
+          admin: accountType
+          }
+        });
+      } catch (e) {
+        console.log(e);
+      }
+      // Update the user's account type in the global state
+      // const updatedUsers = users.map((u) => {
+      //   if (u._id === user._id) {
+      //     u.admin = accountType;
+      //   }
+      //   return u;
+      // });
+      // dispatch({
+      //   type: UPDATE_USERS,
+      //   users: updatedUsers
+      // });
+    }
+  };
 
   return (
     <div className='col-12'>
@@ -20,11 +48,12 @@ export default function UserItem( {user} ) {
         {user.firstName} {user.lastName}
       </h3>
       <p>Email: {user.email}</p>
-      <textbox>Admin: {user.admin ? "admin" : "regular"}</textbox>
-      <button
-      onClick={handleInputChange}
-      >
-        <a href={`/admin/users/${user._id}`}>Edit</a>
+      <p id='accountType'>Account Type: {user.admin}</p>
+
+
+      {/* Button trigger for modal */}
+      <button type='button' className='btn btn-primary' onClick={handleEditClick}>
+        Edit
       </button>
     </div>
   )
--- client/src/pages/admin/adminUsers.js
+++ client/src/pages/admin/adminUsers.js
@@ -1,8 +1,9 @@
 // Admin page that allows user management
 // Import dependencies and queries
-import React from "react";
+import React, {useEffect} from "react";
 import { useQuery } from "@apollo/client";
-import { QUERY_USERS, UPDATE_USERS } from "../../utils/queries";
+import { QUERY_USERS } from "../../utils/queries";
+import { UPDATE_USERS } from "../../utils/actions";
 import UserItem from "../../components/admin/UserItem";
 import { useStoreContext } from "../../contexts/GlobalContext";
 
@@ -10,7 +11,18 @@ export default function AdminUsers() {
   const [state, dispatch] = useStoreContext();
   const { loading, error, data } = useQuery(QUERY_USERS);
   const userData = data?.users || [];
-  console.log(userData);
+
+  // Can be used in the future if we change any user info to re-render info on the page.
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_USERS,
+        users: userData
+      });
+    } else {
+      console.log('No data found');
+    }
+  }, [data, dispatch]);
 
   return (
     <div className='container-fluid'>
