027ab23e0ff1dd234fd4da1abc2d3cfd65e7affd
john-abou <abounassar.john@gmail.com>
2023-04-02 15:25:38 -0400

    Feat: Styling cart, added checkout button styling on the cart item.

--- client/src/components/User/CartItem/index.js
+++ client/src/components/User/CartItem/index.js
@@ -21,21 +21,23 @@ export default function CartItem({ product }) {
   };
   
   return (
-    <div className="flex-row">
-      <div>
-        <img className='cart-img' src={product.image} alt='product'></img>
-      </div>  
-      <div>
-        {product.name}, {product.purchaseCount} @ ${product.price}
-      </div>
-      <div>
-        <span
-          role="img"
-          aria-label="X icon"
-          onClick={clearCart}
-        >
-          ❌
-        </span>
+    <div className="container">
+      <div className="row">
+        <div className="col-2 me-2">
+          <img className='cart-img' src={product.image} alt='product'></img>
+        </div>  
+        <div className="col-8">
+          {product.name},  <strong>${product.price}</strong> x {product.purchaseCount}
+        </div>
+        <div className='ms-sm-3 ms-md-4 col-1'>
+          <span
+            role="img"
+            aria-label="X icon"
+            onClick={clearCart}
+          >
+            ❌
+          </span>
+        </div>
       </div>
     </div>
   );
