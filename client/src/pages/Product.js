db9355ce19b4d7bd1bdb7706a01223fc8e4b764d
john-abou <abounassar.john@gmail.com>
2023-04-02 17:48:19 -0400

    Feat: Adding functionality to the product page add to cart button.

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -3,10 +3,15 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_PRODUCT } from '../utils/queries';
+import { useStoreContext } from '../contexts/GlobalContext';
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../utils/actions';
+import CartContainer from '../components/User/CartContainer';
 
 import PriceChart from '../components/User/PriceChart/priceHistoryChart';
 
 export default function ProductPage() {
+  const [state, dispatch] = useStoreContext();
+  const {cart} = state;
 
   const { id } = useParams();
 
@@ -19,6 +24,24 @@ export default function ProductPage() {
   const [WalmartPriceHistory, setWalmartPriceHistory] = useState([]);
   const [LoblawsPriceHistory, setLoblawsPriceHistory] = useState([]);
 
+  const addToCart = () => {
+    // Determine if the item is in the cart, then add to cart and update quantity
+    const productInCart = state.cart.find((cartProduct) => cartProduct._id === newproduct._id);
+    if (productInCart && productInCart.purchaseCount > 0) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: productInCart._id,
+        purchaseCount: parseInt(productInCart.purchaseCount) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...newproduct, purchaseCount: 1 }
+      });
+    }
+    console.log('cart',state);
+  }
+  
   const { data } = useQuery(QUERY_SINGLE_PRODUCT, {
     variables: { id },
   })
@@ -44,7 +67,7 @@ export default function ProductPage() {
       <div className="d-flex">
         <div className="d-flex flex-column justify-content-center align-items-center p-3" style={{ margin: '20px' }}>
           <img src={newproduct.image} className="mx-auto d-block" width="500" height="500" />
-          <button className="btn btn-primary w-100 mt-3">Add to Cart</button>
+          <button className="btn btn-primary w-100 mt-3" onClick={addToCart}>Add to Cart</button>
         </div>
         <div className="p-3" >
           <div className="d-flex flex-column justify-content-center align-items-center p-3" style={{ margin: '20px' }}>
@@ -67,6 +90,7 @@ export default function ProductPage() {
               <PriceChart key={newproduct._id} priceHistory={LoblawsPriceHistory} comp="Loblaws" lineColour="red" />
             </div>
           </div>
+          <CartContainer />
         </div>
       </div>
     </div>
