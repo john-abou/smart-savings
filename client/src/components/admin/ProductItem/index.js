0520c3d276134f6fbec2783563cbf7be60ed6567
john-abou <abounassar.john@gmail.com>
2023-05-09 20:52:02 -0400

    Feat: Added a delete feature for removing products as an admin.

--- client/src/components/admin/ProductItem/index.js
+++ client/src/components/admin/ProductItem/index.js
@@ -1,12 +1,37 @@
 import React, {useEffect} from 'react';
 import { useStoreContext } from '../../../contexts/GlobalContext';
-import { ADD_TO_CART, TOGGLE_CART, UPDATE_CART_QUANTITY, CLEAR_CART, ADD_TO_INVENTORY, REMOVE_FROM_INVENTORY, INVENTORY_CHECK } from '../../../utils/actions';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { DELETE_ITEM } from '../../../utils/mutations';
+import { UPDATE_PRODUCTS } from '../../../utils/actions';
+import { useNavigate } from 'react-router-dom';
 import './style.css';
 
 export default function ProductItem({ product }) {
   const { name, description, price, quantity, image, _id } = product;
   const [state, dispatch] = useStoreContext();
+
+  const navigate = useNavigate();
+
+  // Mutation for deleting a product
+  const [deleteProduct] = useMutation(DELETE_ITEM);
+  const deleteitem = async (productId) => {
+    await deleteProduct({ variables: { productId } });
+
+    // Update the products in the global state
+    dispatch({
+      type: UPDATE_PRODUCTS,
+      products: state.products.filter((product) => product._id !== _id)
+    });
+  }
+
+  // Use effect to update the page when a product is deleted
+  useEffect(() => {
+    if (state.products.length === 0) {
+      navigate('/admin/products');
+    }
+  }, [state.products.length, navigate]);
+
   
   // Make a function to truncate text
   const truncateText = (text, maxlength) => {
@@ -31,6 +56,7 @@ export default function ProductItem({ product }) {
           <div className='image-container'>
             <img src={image} width="150" height="150" alt={`${name}`}></img>
             <p className='card-price'>CAD: ${price}</p>
+            <button className='btn btn-primary' style={{backgroundColor: '#07689F'}} onClick={() => deleteitem(_id)}>Delete</button>
           </div>
         </div>
         <div className='card-footer'>
