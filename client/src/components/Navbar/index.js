4a7b5bd0df6f235e0e3d9c3d49aed67a45745b05
john-abou <abounassar.john@gmail.com>
2023-05-09 20:17:19 -0400

    Fix: Navbar links spacing

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -37,7 +37,7 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           {!loggedIn ? (
             <div className="d-flex flex-row justify-content-center align-items-center navbar-links">
-              <ul className='navbar-nav navbar-links'>
+              <ul className='navbar-nav'>
                 <li className="nav-item">
                   <Link className="nav-link" to="/login">
                     Login
